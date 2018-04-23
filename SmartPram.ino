ba7ad04a3bbd3f347f899a851a3dc5560fcd2ce5
Michael Barbera <michael.barbera96@gmail.com>
2018-04-24 08:41:54 +1000

    Initial Commit

---
+++ SmartPram.ino
@@ -0,0 +1,94 @@
+/*
+ * Smart Pram
+ * 
+ * Activates brakes when no hands are detected.
+ * 
+ * Created 22 April 2018
+ * 
+ */
+//----------- Defines -----------//
+#define PRESSURETHRESHOLD 0
+#define HANDSOFFTIME 3
+#define BRAKETIME 10
+
+#define relayPin 2
+#define pressurePin A0
+
+#define checkHands 0
+#define activateBrake 1
+#define releaseBrake 2
+
+bool handOff_detected = false;
+
+unsigned long handsOffTime;
+unsigned long brakeStarted;
+
+
+byte state = 0;
+//-------------------------------//
+//------- Helper Functions ------//
+void PinSetup(void) {
+  pinMode(relayPin, INPUT);
+  pinMode(pressurePin, OUTPUT);
+}
+
+bool HandsOn(void) {
+  return(analogRead(pressurePin) >= PRESSURETHRESHOLD);
+}
+
+void ActuateBrake(void) {
+  digitalWrite(relayPin, HIGH);
+}
+
+void ReleaseBrake(void) {
+  digitalWrite(relayPin, LOW);
+}
+
+void InitPram(void) {
+  ReleaseBrake();
+}
+
+//-------------------------------//
+//------------ Setup ------------//
+void setup() {
+  PinSetup();
+  InitPram();
+  state = 0;
+}
+//-------------------------------//
+//------------ Loop -------------//
+void loop() { 
+  switch(state) {
+    case checkHands:
+      if(handOff_detected) {
+        if(HandsOn()) {
+          handOff_detected = false;
+        } else {
+          if(millis() - handsOffTime >= HANDSOFFTIME) {
+            state = activateBrake;
+            ActuateBrake();
+            brakeStarted = millis();
+          }
+        }
+      } else {
+        if(!HandsOn()) {
+          handOff_detected = true;
+          handsOffTime = millis();
+        }   
+      }
+      break;
+    case activateBrake:
+      if(millis() - brakeStarted >= BRAKETIME || HandsOn()) {
+        state = releaseBrake;
+      } 
+      break;
+    case releaseBrake:
+      ReleaseBrake();
+      state = checkHands;
+      break;
+    default:
+      state = checkHands;
+  }
+}
+//-------------------------------//
+
